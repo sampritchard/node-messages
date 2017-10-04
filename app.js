7f35ca102e7aeaf29822c899e0ae8cd3cbca4a1c
Sam Pritchard <sampritchard@msn.com>
2017-10-04 10:46:12 +0100

    Removed unncessary cookieParser module

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 var path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 
 require('./models/Message');
 
